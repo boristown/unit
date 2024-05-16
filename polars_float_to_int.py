49f93757c883ef33054e594499bf8b19b73fc830
AK自动机 <47687653+boristown@users.noreply.github.com>
2024-05-16 22:36:47 +0800

    Not None

--- polars_float_to_int.py
+++ polars_float_to_int.py
@@ -5,6 +5,8 @@ class ploars_float_to_int:
     def convertAuto(df:pl.DataFrame, col:str)->pl.DataFrame:
         min_v = df[col].min()
         max_v = df[col].max()
+        if min_v is None or max_v is None:
+            return df
         if min_v >= 0 and max_v <= np.iinfo(np.uint8).max:
             df = df.with_columns(df[col].cast(pl.UInt8))
         elif min_v >= 0 and max_v <= np.iinfo(np.uint16).max:
@@ -28,7 +30,7 @@ class ploars_float_to_int:
 if __name__ == "__main__":
     df = pl.DataFrame({
         "a": [1.0, 2.0, 3.3, -123123, 234234985734034],
-        "b": [1, 2, 3, 4, 5],
+        "b": [1, 2, 3, 4, None],
         "c": ["a", "b", "c", "d", "e"]
         })
     print(ploars_float_to_int.convertAuto(df, "a"))
