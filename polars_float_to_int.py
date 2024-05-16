c61c60b456926cf5e614cb1c46771f3762d839ef
AK自动机 <47687653+boristown@users.noreply.github.com>
2024-05-16 22:23:45 +0800

    polars float to int

---
+++ polars_float_to_int.py
@@ -0,0 +1,35 @@
+import polars as pl
+import numpy as np
+
+class ploars_float_to_int:
+    def convertAuto(df:pl.DataFrame, col:str)->pl.DataFrame:
+        min_v = df[col].min()
+        max_v = df[col].max()
+        if min_v >= 0 and max_v <= np.iinfo(np.uint8).max:
+            df = df.with_columns(df[col].cast(pl.UInt8))
+        elif min_v >= 0 and max_v <= np.iinfo(np.uint16).max:
+            df = df.with_columns(df[col].cast(pl.UInt16))
+        elif min_v >= 0 and max_v <= np.iinfo(np.uint32).max:
+            df = df.with_columns(df[col].cast(pl.UInt32))
+        elif min_v >= 0 and max_v <= np.iinfo(np.uint64).max:
+            df = df.with_columns(df[col].cast(pl.UInt64))
+        elif min_v >= np.iinfo(np.int8).min and max_v <= np.iinfo(np.int8).max:
+            df = df.with_columns(df[col].cast(pl.Int8))
+        elif min_v >= np.iinfo(np.int16).min and max_v <= np.iinfo(np.int16).max:
+            df = df.with_columns(df[col].cast(pl.Int16))
+        elif min_v >= np.iinfo(np.int32).min and max_v <= np.iinfo(np.int32).max:
+            df = df.with_columns(df[col].cast(pl.Int32))
+        elif min_v >= np.iinfo(np.int64).min and max_v <= np.iinfo(np.int64).max:
+            df = df.with_columns(df[col].cast(pl.Int64))
+        else:
+            df = df.with_columns(df[col].cast(pl.Float64))
+        return df
+    
+if __name__ == "__main__":
+    df = pl.DataFrame({
+        "a": [1.0, 2.0, 3.3, -123123, 234234985734034],
+        "b": [1, 2, 3, 4, 5],
+        "c": ["a", "b", "c", "d", "e"]
+        })
+    print(ploars_float_to_int.convertAuto(df, "a"))
+    print(ploars_float_to_int.convertAuto(df, "b"))
